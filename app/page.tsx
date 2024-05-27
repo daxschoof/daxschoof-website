ca4222718edf380d77c2561610a19dee7d1dbd8c
Dax S <daxschoof@gmail.com>
2024-05-27 09:16:06 -0500

    Page style & base setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import Card from "@/components/card";
+import NavigationBar from "@/components/navigation";
+import { Fragment } from "react";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <br />
-      <p>This is my page</p>
-    </div>
+    <Fragment>
+      <div className="mainPage">
+        <Card>
+          <p>Dax Schoof</p>
+          <p>Page under construction!</p>
+        </Card>
+      </div>
+
+      <NavigationBar />
+    </Fragment>
   );
 }
---
+++ components/navigation.tsx
@@ -0,0 +1,12 @@
+const NavigationBar = () => {
+  return (
+    <div className="navigator">
+      <a href="daxschoof.dev/resume">Resume</a>
+      <a href="daxschoof.dev/timeline">Timeline</a>
+      <a href="https://www.linkedin.com/in/dax-schoof/">LinkedIn</a>
+      <a href="daxschoof.dev/contactme">Contact Me</a>
+    </div>
+  );
+};
+
+export default NavigationBar;

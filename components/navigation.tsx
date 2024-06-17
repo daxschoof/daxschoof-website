027e64e668ce47e4fa019d6fa66237aa03b05650
Dax S <daxschoof@gmail.com>
2024-06-16 21:48:24 -0500

    Fixing length of navbar items

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -4,8 +4,8 @@ const NavigationBar = () => {
   return (
     <div className="navigator">
       <Link href="/">Home</Link>
-      <Link href="/about_me">Stuff I&apos;m Working On</Link>
-      <Link href="/contact_me">Links & Contact Info</Link>
+      <Link href="/about_me">About Me</Link>
+      <Link href="/contact_me">Links & Contact</Link>
     </div>
   );
 };
